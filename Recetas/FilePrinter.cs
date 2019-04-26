4a31e9bf59e241aba324c4d9599ad9a6671b8311
perez5010 <perez5010@hotmail.com>
2019-04-26 17:22:50 -0300

    Agrego lo de interfaces

---
+++ Recetas/FilePrinter.cs
@@ -0,0 +1,14 @@
+
+using System;
+using System.IO;
+
+namespace Full_GRASP_And_SOLID
+{
+    public class FilePrinter : IPrinter
+    {
+        public void PrintRecipe(Recipe recipe)
+        {
+            File.WriteAllText("Recipe.txt", recipe.GetTextToPrint());
+        }
+    }
+}
