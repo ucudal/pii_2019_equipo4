a75bd6c3877bf7be20aeaafe30db6dd348211f85
joaquinpereyra <42189479+joaquinpereyra@users.noreply.github.com>
2019-05-31 20:18:35 -0300

    pagina All Projects
    cambios a los cshtml

---
+++ Proyecto/Pages/AllProjects.cshtml.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Proyecto.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Proyecto.Pages
+{
+    public class AllProjectsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+        private Proyecto.Models.ProjectContext _context;
+        
+        public void projectdetails(Proyecto.Models.ProjectContext context)
+        {
+            _context =context;
+        }
+          
+        
+        public Project Project { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+        
+            if (id == null)
+            {
+                Project = await _context.Project.FirstOrDefaultAsync();
+            }
+            else
+            {
+                Project = await _context.Project.FirstOrDefaultAsync(m => m.ProjectID == id);
+            }
+
+            if (Project == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
--- Proyecto/Pages/YourProjects.cshtml.cs
+++ Proyecto/Pages/YourProjects.cshtml.cs
@@ -12,40 +12,6 @@ namespace Proyecto.Pages
         public void OnGet()
         {            
         }
-        /*private Proyecto.Models.ProjectContext _context;
         
-        public void projectdetails(Proyecto.Models.ProjectContext context)
-        {
-            _context =context;
-        }
-
-       // public DetailsModel(Proyecto.Models.ProjectContext context)
-        //{
-        //  _context = context;
-        //}
-          
-        }
-        /*public Project Project { get; set; }
-
-        public async Task<IActionResult> OnGetAsync(int? id)
-        {
-        
-            if (id == null)
-            {
-                Project = await _context.Project.FirstOrDefaultAsync();
-            }
-            else
-            {
-                Project = await _context.Project.FirstOrDefaultAsync(m => m.ProjectID == id);
-            }
-
-            if (Project == null)
-            {
-                return NotFound();
-            }
-            return Page();
-        }
-        */
-
     }
 }
