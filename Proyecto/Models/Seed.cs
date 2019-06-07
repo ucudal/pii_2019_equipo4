40c169d218a8b4cb5c3b71e958f6f32ae49e84e0
fabian1906 <fabian1506l@gmail.com>
2019-06-07 19:34:26 -0300

    arreglos en seed

--- Proyecto/Models/Seed.cs
+++ Proyecto/Models/Seed.cs
@@ -7,182 +7,191 @@ namespace Proyecto.Models
 {
     public static class SeedData
     {
+        private static void SeedRoleLevels(ProjectContext context)
+        {
+            // Look for any RoleLevel.
+            if (context.RoleLevel.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.RoleLevel.AddRange(
+                new RoleLevel
+                {
+                    RolLvlDsc = "Básico"
+                },
+
+                new RoleLevel
+                {
+                    RolLvlDsc = "Avanzado"
+                }
+            );
+            context.SaveChanges();
+        }
+
+        private static void SeedRoles(ProjectContext context)
+        {
+            // Look for any Role.
+            if (context.Role.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Role.AddRange(
+                new Role
+                {
+                    RolDsc = "Foto fija",
+                    RolLvlId = 1
+                },
+
+                new Role
+                {
+                    RolDsc = "Asistente de cámara",
+                    RolLvlId = 1
+                },
+
+                new Role
+                {
+                    RolDsc = "Asistente de producción",
+                    RolLvlId = 1
+                },
+
+                new Role
+                {
+                    RolDsc = "Asistente de dirección",
+                    RolLvlId = 1
+                },
+
+                new Role
+                {
+                    RolDsc = "Asistente de arte (escenografía, vestuario, utilería)",
+                    RolLvlId = 1
+                },
+
+                new Role
+                {
+                    RolDsc = "Sonidista",
+                    RolLvlId = 1
+                },
+
+                new Role
+                {
+                    RolDsc = "Editor",
+                    RolLvlId = 1
+                },
+
+                new Role
+                {
+                    RolDsc = "Redactor creativo",
+                    RolLvlId = 1
+                },
+
+                new Role
+                {
+                    RolDsc = "Presentador / conductor",
+                    RolLvlId = 1
+                },
+
+                new Role
+                {
+                    RolDsc = "Ilustrador",
+                    RolLvlId = 1
+                },
+
+                new Role
+                {
+                    RolDsc = "Diseñador gráfico",
+                    RolLvlId = 1
+                },
+
+                new Role
+                {
+                    RolDsc = "Operador de Cabina 02",
+                    RolLvlId = 1
+                },
+
+                new Role
+                {
+                    RolDsc = "Operador de Cabina 03 y Estudio de Radio",
+                    RolLvlId = 1
+                },
+
+                new Role
+                {
+                    RolDsc = "Foto fija",
+                    RolLvlId = 2
+                },
+
+                new Role
+                {
+                    RolDsc = "Cámara y asistente de cámara",
+                    RolLvlId = 2
+                },
+
+                new Role
+                {
+                    RolDsc = "Cámara 360",
+                    RolLvlId = 2
+                },
+
+                new Role
+                {
+                    RolDsc = "Postproductor de imagen",
+                    RolLvlId = 2
+                },
+
+                new Role
+                {
+                    RolDsc = "Editor",
+                    RolLvlId = 2
+                },
+
+                new Role
+                {
+                    RolDsc = "Sonidista",
+                    RolLvlId = 2
+                },
+
+                new Role
+                {
+                    RolDsc = "Postproductor de sonido",
+                    RolLvlId = 2
+                },
+
+                new Role
+                {
+                    RolDsc = "Redactor creativo",
+                    RolLvlId = 2
+                },
+
+                new Role
+                {
+                    RolDsc = "Presentador / conductor",
+                    RolLvlId = 2
+                },
+
+                new Role
+                {
+                    RolDsc = "Animador / infografista",
+                    RolLvlId = 2
+                },
+
+                new Role
+                {
+                    RolDsc = "Operador de Cabina 01 Estudio de Grabación",
+                    RolLvlId = 2
+                }
+            );
+            context.SaveChanges();
+        }
         public static void Initialize(IServiceProvider serviceProvider)
         {
             using (var context = new ProjectContext(
                 serviceProvider.GetRequiredService<
                     DbContextOptions<ProjectContext>>()))
             {
-                // Look for any RoleLevel.
-                if (context.RoleLevel.Any())
-                {
-                    return;   // DB has been seeded
-                }
-
-                context.RoleLevel.AddRange(
-                    new RoleLevel
-                    {
-                        RolLvlDsc = "Básico"
-                    },
-
-                    new RoleLevel
-                    {
-                        RolLvlDsc = "Avanzado"
-                    }
-                );
-
-                // Look for any Role.
-                if (context.Role.Any())
-                {
-                    return;   // DB has been seeded
-                }
-
-                context.Role.AddRange(
-                    new Role
-                    {
-                        RolDsc = "Foto fija",
-                        RolLvlId = 1
-                    },
-
-                    new Role
-                    {
-                        RolDsc = "Asistente de cámara",
-                        RolLvlId = 1
-                    },
-
-                    new Role
-                    {
-                        RolDsc = "Asistente de producción",
-                        RolLvlId = 1
-                    },
-
-                    new Role
-                    {
-                        RolDsc = "Asistente de dirección",
-                        RolLvlId = 1
-                    },
-
-                    new Role
-                    {
-                        RolDsc = "Asistente de arte (escenografía, vestuario, utilería)",
-                        RolLvlId = 1
-                    },
-
-                    new Role
-                    {
-                        RolDsc = "Sonidista",
-                        RolLvlId = 1
-                    },
-
-                    new Role
-                    {
-                        RolDsc = "Editor",
-                        RolLvlId = 1
-                    },
-
-                    new Role
-                    {
-                        RolDsc = "Redactor creativo",
-                        RolLvlId = 1
-                    },
-
-                    new Role
-                    {
-                        RolDsc = "Presentador / conductor",
-                        RolLvlId = 1
-                    },
-
-                    new Role
-                    {
-                        RolDsc = "Ilustrador",
-                        RolLvlId = 1
-                    },
-
-                    new Role
-                    {
-                        RolDsc = "Diseñador gráfico",
-                        RolLvlId = 1
-                    },
-
-                    new Role
-                    {
-                        RolDsc = "Operador de Cabina 02",
-                        RolLvlId = 1
-                    },
-
-                    new Role
-                    {
-                        RolDsc = "Operador de Cabina 03 y Estudio de Radio",
-                        RolLvlId = 1
-                    },
-
-                    new Role
-                    {
-                        RolDsc = "Foto fija",
-                        RolLvlId = 2
-                    },
-
-                    new Role
-                    {
-                        RolDsc = "Cámara y asistente de cámara",
-                        RolLvlId = 2
-                    },
-
-                    new Role
-                    {
-                        RolDsc = "Cámara 360",
-                        RolLvlId = 2
-                    },
-
-                    new Role
-                    {
-                        RolDsc = "Postproductor de imagen",
-                        RolLvlId = 2
-                    },
-
-                    new Role
-                    {
-                        RolDsc = "Editor",
-                        RolLvlId = 2
-                    },
-
-                    new Role
-                    {
-                        RolDsc = "Sonidista",
-                        RolLvlId = 2
-                    },
-
-                    new Role
-                    {
-                        RolDsc = "Postproductor de sonido",
-                        RolLvlId = 2
-                    },
-
-                    new Role
-                    {
-                        RolDsc = "Redactor creativo",
-                        RolLvlId = 2
-                    },
-
-                    new Role
-                    {
-                        RolDsc = "Presentador / conductor",
-                        RolLvlId = 2
-                    },
-
-                    new Role
-                    {
-                        RolDsc = "Animador / infografista",
-                        RolLvlId = 2
-                    },
-
-                    new Role
-                    {
-                        RolDsc = "Operador de Cabina 01 Estudio de Grabación",
-                        RolLvlId = 2
-                    }
-                );
-                context.SaveChanges();
+                SeedRoleLevels(context);
+                SeedRoles(context);
             }
         }
     }
